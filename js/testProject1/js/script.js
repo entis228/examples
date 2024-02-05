67433138f413e7a3a63725b40525fd9feb7b30a0
Entis <noreply@entiscorp.com>
2024-02-05 11:17:15 +0000

    Improve film survey

--- js/testProject1/js/script.js
+++ js/testProject1/js/script.js
@@ -1,5 +1,23 @@
 "use strict";
 
+const validatePrompt = (prompt) => {
+  if (prompt) {
+    if (typeof (prompt) === "string") {
+      if (prompt.length < 50) {
+        return true;
+      } else {
+        alert("Your answer is more than 50 symbols, please enter another smaller name");
+      }
+    }
+    if (typeof (prompt) === "number") {
+      return true;
+    }
+  } else {
+    alert("Your answer is blank or invalid")
+  }
+  return false;
+}
+
 const numberOfFilms = prompt("How many films did you watch?", "");
 
 const personalMoviesDb = {
@@ -10,7 +28,39 @@ const personalMoviesDb = {
   privat: false
 };
 
-personalMoviesDb.movies[prompt("One of the last watched films", "")] = prompt("Mark (from 1 to 10)", "");
-personalMoviesDb.movies[prompt("One of the last watched films", "")] = prompt("Mark (from 1 to 10)", "");
+for (let i = 0; i < 2; i++) {
+  let filmName;
+  let filmRate;
+  while (true) {
+    filmName = prompt(`Film #${i + 1}: One of the last watched films`, "");
+    if (!validatePrompt(filmName)) {
+      continue;
+    }
+    filmRate = +prompt(`Film #${i + 1}: Mark (from 1 to 10)`, "");
+    if (!validatePrompt(filmRate)) {
+      continue;
+    }
+    break;
+  }
+
+  personalMoviesDb.movies[filmName] = filmRate;
+}
+
+const filmCount = personalMoviesDb.count;
+if (filmCount >= 0 && filmCount < 10) {
+  alert("Not so many films are watched");
+} else {
+  if (filmCount >= 10 && filmCount < 30) {
+    alert("You are a nice movie buff");
+  } else {
+    if (filmCount >= 30) {
+      alert("You very like to watch movies, you spent plenty of time on it");
+    } else {
+      alert("Error in film count");
+    }
+  }
+}
+
 
 console.log(personalMoviesDb);
+

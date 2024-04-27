f050969f068520619f2ac0e36c10e5cb953d7edd
Entis <noreply@entiscorp.com>
2024-04-27 21:31:13 +0100

    Refactor, add some functions

--- js/regularJs/js/script.js
+++ js/regularJs/js/script.js
@@ -1,97 +1,99 @@
 "use strict";
 
-let numberOfFilms;
-
 const personalMoviesDb = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {},
   genres: [],
-  privat: false
+  privat: false,
+  start: function () {
+    this.count = this.promptTillValid("How many films did you watch?", "number", false);
+  },
+  saveFilms: function () {
+    for (let i = 0; i < this.count; i++) {
+      let filmName = this.promptTillValid(`Film #${i + 1}: One of the last watched films`, "string", false);
+      while (filmName.length >= 50) {
+        if (filmName.length >= 50) {
+          alert("Your answer is more than 50 symbols, please enter another smaller name");
+        }
+        filmName = this.promptTillValid(`Film #${i + 1}: One of the last watched films`, "string", false);
+      }
+      this.movies[filmName] = this.promptTillValid(`Film #${i + 1}: Mark (from 1 to 10)`, "number", false);
+    }
+  },
+  promptTillValid: function (question, responseType, isBlank) {
+    const checkIfBlank = (value) => {
+      return value && value.length === 0;
+    }
+    if (typeof question !== "string" || checkIfBlank(question)) {
+      throw new Error("Question is blank or not a string");
+    }
+    if (typeof responseType !== "string" || checkIfBlank(question)) {
+      throw new Error("Type is blank or not a string");
+    }
+    if (typeof isBlank !== "boolean") {
+      throw new Error("IsBlank is not a boolean");
+    }
+    while (true) {
+      const stringResponse = prompt(question, "");
+      if (!isBlank && checkIfBlank(stringResponse)) {
+        alert("Prompt value must be not blank");
+        continue;
+      }
+      if (responseType === "number") {
+        const numberValue = +stringResponse;
+        if (isNaN(numberValue)) {
+          alert("Invalid prompt, the value is not a number");
+          continue;
+        }
+        return numberValue;
+      } else {
+        return stringResponse;
+      }
+    }
+  },
+  detectLevel: function () {
+    const filmCount = this.count;
+    if (filmCount >= 0 && filmCount < 10) {
+      alert("Not so many films are watched");
+    } else {
+      if (filmCount >= 10 && filmCount < 30) {
+        alert("You are a nice movie buff");
+      } else {
+        if (filmCount >= 30) {
+          alert("You very like to watch movies, you spent plenty of time on it");
+        } else {
+          alert("Error in film count");
+        }
+      }
+    }
+  },
+  showMyDb: function () {
+    if (!this.privat) {
+      console.log(this);
+    }
+  },
+  writeYourGenres: function () {
+    for (let i = 0; i < 3; i++) {
+      this.genres[i] = this.promptTillValid(`Your favourite genre #${i + 1}`, "string", true);
+    }
+    this.genres.forEach((genre, index) => console.log(`Любимый жанр №${index + 1} - это ${genre}`))
+  },
+  toggleVisibleMyDb: function () {
+    this.privat = !this.privat;
+  }
 };
 
-const start = () => {
-  numberOfFilms = +prompt("How many films did you watch?", "");
-  while (numberOfFilms == null || isNaN(numberOfFilms)) {
-    alert("Invalid input");
-    numberOfFilms = +prompt("How many films did you watch?", "");
-  }
-  personalMoviesDb.count = numberOfFilms;
-}
+personalMoviesDb.start();
 
-const saveFilms = () => {
-  for (let i = 0; i < numberOfFilms; i++) {
-    let filmName;
-    let filmRate;
-    while (true) {
-      filmName = prompt(`Film #${i + 1}: One of the last watched films`, "");
-      if (!validatePrompt(filmName)) {
-        continue;
-      }
-      filmRate = +prompt(`Film #${i + 1}: Mark (from 1 to 10)`, "");
-      if (!validatePrompt(filmRate)) {
-        continue;
-      }
-      break;
-    }
+personalMoviesDb.saveFilms();
 
-    personalMoviesDb.movies[filmName] = filmRate;
-  }
-}
+personalMoviesDb.detectLevel();
 
-const validatePrompt = (prompt) => {
-  if (prompt) {
-    if (typeof (prompt) === "string") {
-      if (prompt.length < 50) {
-        return true;
-      } else {
-        alert("Your answer is more than 50 symbols, please enter another smaller name");
-      }
-    }
-    if (typeof (prompt) === "number") {
-      return true;
-    }
-  } else {
-    alert("Your answer is blank or invalid")
-  }
-  return false;
-}
+personalMoviesDb.showMyDb();
 
-const detectLevel = () => {
-  const filmCount = personalMoviesDb.count;
-  if (filmCount >= 0 && filmCount < 10) {
-    alert("Not so many films are watched");
-  } else {
-    if (filmCount >= 10 && filmCount < 30) {
-      alert("You are a nice movie buff");
-    } else {
-      if (filmCount >= 30) {
-        alert("You very like to watch movies, you spent plenty of time on it");
-      } else {
-        alert("Error in film count");
-      }
-    }
-  }
-}
+personalMoviesDb.writeYourGenres();
 
-const showMyDb = (hidden) => {
-  if (!hidden) {
-    console.log(personalMoviesDb);
-  }
-}
+personalMoviesDb.toggleVisibleMyDb();
 
-const writeYourGenres = () => {
-  for (let i = 0; i < 3; i++) {
-    personalMoviesDb.genres[i] = prompt(`Your favourite genre #${i + 1}`);
-  }
-}
-
-start();
-
-saveFilms();
-
-detectLevel();
-
-showMyDb(personalMoviesDb.privat);
-
-writeYourGenres();
+personalMoviesDb.showMyDb();

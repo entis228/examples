e07fb3915a045895e9c24427282df72d8e06d1e1
Entis <noreply@entiscorp.com>
2024-05-25 11:30:56 +0100

    Add modal window and timer

--- js/03_FoodDist/js/script.js
+++ js/03_FoodDist/js/script.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  main();
+  tabs();
+  timer();
+  modal();
 });
 
-const main = () => {
+const tabs = () => {
   const items = document.querySelectorAll('.tabheader__item'),
     tabContents = document.querySelectorAll(".tabcontent"),
     itemContainer = document.querySelector('.tabheader__items');
@@ -38,4 +40,108 @@ const main = () => {
       });
     }
   });
+}
+
+const timer = () => {
+  const deadline = new Date(new Date().getTime()+9999999).toISOString();
+  let intervalId = setInterval(updateTimer, 1000);
+  updateTimer();
+
+  function updateTimer() {
+    const getTimeFromDeadline = (deadline) => {
+      let millis = new Date(deadline).getTime() - new Date().getTime();
+      let days = 0, hours = 0, minutes = 0, seconds = 0;
+      if (millis > 0) {
+        days = Math.floor(millis / (1000 * 60 * 60 * 24));
+        hours = Math.floor((millis % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        minutes = Math.floor((millis % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((millis % (1000 * 60)) / 1000);
+      }
+
+      return {
+        millis,
+        days,
+        hours,
+        minutes,
+        seconds
+      }
+    }
+
+    const updateSelector = (timeValues) => {
+      const addZeroIfNeeded = (value) => {
+        let stringValue = value.toString();
+        if (stringValue.length < 2) {
+          return '0' + stringValue;
+        }
+        return stringValue;
+      }
+
+      const timer = document.querySelector(".timer"),
+        days = timer.querySelector("#days"),
+        hours = timer.querySelector("#hours"),
+        minutes = timer.querySelector("#minutes"),
+        seconds = timer.querySelector("#seconds");
+
+      days.textContent = addZeroIfNeeded(timeValues.days);
+      hours.textContent = addZeroIfNeeded(timeValues.hours);
+      minutes.textContent = addZeroIfNeeded(timeValues.minutes);
+      seconds.textContent = addZeroIfNeeded(timeValues.seconds);
+    }
+
+    const t = getTimeFromDeadline(deadline);
+    if (t.millis <= 0) {
+      clearInterval(intervalId);
+    }
+    updateSelector(t);
+  }
+}
+
+const modal = ()=>{
+  const openModalButtons = document.querySelectorAll('[data-modal]'),
+    modalWindow = document.querySelector('.modal'),
+    closeButton = modalWindow.querySelector('[data-close]');
+
+  openModalButtons.forEach(button => button.addEventListener('click', (event) => {
+    event.preventDefault();
+    openModalWindow();
+  }));
+
+  closeButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    closeModalWindow();
+  });
+
+  modalWindow.addEventListener('click', (event) => {
+    event.preventDefault();
+    if(event.target === modalWindow) {
+      closeModalWindow();
+    }
+  });
+
+  document.addEventListener('keydown', event => {
+    if(event.code === 'Escape' && modalWindow.classList.contains('show')) {
+      closeModalWindow();
+    }
+  });
+
+  const closeModalWindow = ()=>{
+    modalWindow.classList.toggle('show');
+    document.body.style.overflow = '';
+  }
+
+  const openModalWindow = () =>{
+    modalWindow.classList.toggle('show');
+    document.body.style.overflow = 'hidden';
+  }
+
+  const onScroll = () => {
+    if(window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight-1){
+      openModalWindow();
+      window.removeEventListener('scroll', onScroll);
+    }
+  }
+
+  setTimeout(openModalWindow, 3000);
+
+  window.addEventListener('scroll', onScroll);
 }
